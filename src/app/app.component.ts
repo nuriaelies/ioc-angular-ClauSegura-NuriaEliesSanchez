82335aa0f78c48d4a4c14bf307b79afb09a45cc8
NúriaElies <nuriaeliessanchez@gmail.com>
2026-02-28 19:31:10 +0100

    Exercici 3.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ioc-angular-ClauSegura-NuriaEliesSanchez';
+  
+  constructor(){
+	console.log('App ClauSegura confirmació d’arrancada correcta')
+  }
 }
