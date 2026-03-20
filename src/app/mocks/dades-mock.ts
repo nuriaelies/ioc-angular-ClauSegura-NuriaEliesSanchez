8ac437a84723eefe7cf49d8f0ae529d01cea5aa8
NúriaElies <nuriaeliessanchez@gmail.com>
2026-03-20 20:46:21 +0100

    dades-mock.ts

---
+++ src/app/mocks/dades-mock.ts
@@ -0,0 +1,14 @@
+import { Element } from '../models/element.model';
+
+export const DADES_MOCK: Element[] = [
+  { id: 1, name: 'Gmail',      value: 12, notes: 'ContrasenyaGmail' },
+  { id: 2, name: 'Instagram',  value: 16, notes: 'ContrasenyaInstagram' },
+  { id: 3, name: 'Facebook',   value: 10, notes: 'ContrasenyaFacebook' },
+  { id: 4, name: 'Amazon',     value: 14, notes: 'ContrasenyaAmazon' },
+  { id: 5, name: 'Netflix',    value: 18, notes: 'ContrasenyaNetflix' },
+  { id: 6, name: 'Spotify',    value: 20, notes: 'ContrasenyaSpotify' },
+  { id: 7, name: 'GitHub',     value: 15, notes: 'ContrasenyaGitHub' },
+  { id: 8, name: 'Microsoft',  value: 22, notes: 'ContrasenyaMicrosoft' },
+  { id: 9, name: 'Steam',      value: 16, notes: 'ContrasenyaSteam' },
+  { id: 10, name: 'AppleID',   value: 24, notes: 'ContrasenyaAppleID' }
+];
